7eb47676df8feeeeae5d8bb9b4c23499e38b0f3a
github_username <65722656+MayankGahtori@users.noreply.github.com>
2022-02-04 21:00:58 +0530

    dd

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Button from './Button';
+import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Button />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
