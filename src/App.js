c420f3c8b5bdcc156fb2c2badc99b42d5e609265
github_username <65722656+MayankGahtori@users.noreply.github.com>
2022-02-06 13:09:33 +0530

    yy

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import Home from './components/pages/Home';
+
 import About from './components/pages/About';
-import Contact from './components/pages/Contact';
+
 function App() {
   return (
     <div>
-      <Home />
+      
       <About />
-      <Contact />
+ 
     </div>
   );
 }
