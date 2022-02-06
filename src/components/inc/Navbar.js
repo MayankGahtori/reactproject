06156bb5a4b3555e870071146750cda1c60f4adc
github_username <65722656+MayankGahtori@users.noreply.github.com>
2022-02-06 20:19:11 +0530

    e

--- src/components/inc/Navbar.js
+++ src/components/inc/Navbar.js
@@ -5,12 +5,14 @@ function Navbar() {
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
                 <img src="components/images/R.png" alt="" width="30" height="24" class="d-inline-block align-text-top" />
-
+                <right>
+                    
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
                         <Link to="/" class="nav-link active">Home</Link>
                     </li>
                 </ul>
+                </right>
              </div>
         </nav>
     );
