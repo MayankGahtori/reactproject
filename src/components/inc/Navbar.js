b695f8f12ea20ff64110c833f4ae319fab3c8317
github_username <65722656+MayankGahtori@users.noreply.github.com>
2022-02-06 20:16:15 +0530

    zc

--- src/components/inc/Navbar.js
+++ src/components/inc/Navbar.js
@@ -2,7 +2,17 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 function Navbar() {
     return(
-        
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <img src="components/images/R.png" alt="" width="30" height="24" class="d-inline-block align-text-top" />
+
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <Link to="/" class="nav-link active">Home</Link>
+                    </li>
+                </ul>
+             </div>
+        </nav>
     );
 }
 
