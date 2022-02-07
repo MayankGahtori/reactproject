d2ac0c263252fa8f9294fee508748f6758ece863
github_username <65722656+MayankGahtori@users.noreply.github.com>
2022-02-07 19:38:36 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import Navbar from './components/inc/Navbar';
-import Home from './components/pages/Home';
+
 import About from './components/pages/About';
-import Contact from './components/pages/Contact';
+
 import {BrowserRouter as Router} from 'react-router-dom';
 function App() {
   return (
     <Router>
       <div>
         <Navbar />
-        <Home />
+       
         <About />
-        <Contact />
+       
       </div>
     </Router>
     
