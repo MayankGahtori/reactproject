68d26da694563b341b6101501cb1d12d82c04025
github_username <65722656+MayankGahtori@users.noreply.github.com>
2022-02-08 17:12:05 +0530

    gp

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -10,8 +10,19 @@ function Aboutus() {
                         <div className="col-md-12" text-left>
                             <h1 className="main-heading">Who Are We?</h1>
                             <div className="underline"></div>
-                            <p>
-                            The Confederation of Global Innovators (also known as CGI) is a platform created for the demographic dividend of our nation. We strongly believe that today our countryâ€™s youth, which comprises more than 65% of the total population, is the biggest asset for our country. From the point of inception, the CGI, as a community, has worked tirelessly in the field of bridging the gap between the stakeholders of development (like students, professional aspirants etc) and the opportunities like the corporate housesThe CGI is created to serve as a platform to connect innovators, stakeholders of development and the student community.ðŸ˜Š
+                            <p className="text">
+                            The Confederation of Global Innovators (also known as CGI)<br/> 
+                            is a platform created for the demographic dividend of our<br/> 
+                            nation. We strongly believe that today our countryâ€™s<br/>
+                            youth, which comprises more than 65% of the total<br/> 
+                            population, is the biggest asset for our country. From the<br/> 
+                            point of inception, the CGI, as a community, has worked<br/>
+                            tirelessly in the field of bridging the gap between the <br/>
+                            stakeholders of development (like students, professional<br/>
+                            aspirants etc) and the opportunities like the corporate<br/>
+                            housesThe CGI is created to serve as a platform to connect<br/>
+                            innovators, stakeholders of development and the student<br/>
+                            community.ðŸ˜Š
                             </p>
                         </div>
                     </div>
