9baf61f5d6c4f14f11f2d5236d6f97177ef2f4b2
github_username <65722656+MayankGahtori@users.noreply.github.com>
2022-02-09 17:31:48 +0530

    hh

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,6 +5,7 @@ import Slider3 from '../images/Slider3.jpg';
 import Slider4 from '../images/Slider4.jpg';
 import Slider5 from '../images/Slider5.jpg';
 import nilesh from '../images/nilesh.jpg';
+import geetika from '../images/geetika.jpg';
 
 function Aboutus() {
     return(
@@ -59,8 +60,8 @@ function Aboutus() {
                                 <h5 class="main-heading">Global Approach</h5>
                                 <p class="text">
                                 CGI is a global accelerator that provides a<br />
-                                nurturing holistic environment for startups<br/> 
-                                to attain their maximum potential. A platform supporting all kinds of business,<br/> 
+                                nurturing holistic environment for startups
+                                to attain their maximum potential. A platform supporting all kinds of business,
                                 technical, scientific, agricultural,<br/> 
                                 educational and what not.
                                 </p>
@@ -99,7 +100,7 @@ function Aboutus() {
                 <div className="container">
                     <div className="row">
                         <div class="text-center">
-                            <img src={nilesh} class="img-circle" alt="cinque terre"/>
+                            <img src={nilesh} className="img-circle" alt="cinque terre"/>
                             <h5>Nilesh Sinha</h5>
                             <h6>Chaiman</h6>
                             <p className="text">
@@ -118,6 +119,23 @@ function Aboutus() {
                             on global perspective (like: Blockchain, IoT, Big Data etc.) On his mission of Gamification of Innovation, he has so far been 
                             a guest speaker at 40+ schools, colleges and educational institutions impacting the life of more than Sixty thousand learners 
                             and innovators.
+                            </p>
+                            <img src={geetika} className="img-circle" alt="cinque terre"/>
+                            <h5>Er. Geetika Pandey</h5>
+                            <h6>President</h6>
+                            <p className="text">
+                            Completed Btech-CSE in the year 2016 and has the vibrant experience in IT field. Also has 3+ years’ experience in corporate world. Her goal is to hold a
+                            Senior Corporate position along with being a Philanthropist. She had a very sound communication and Technical skills and regularly participate as an 
+                            invited/guest delegate in many IT Conclave, Summit and Sessions organized by FICCI, CII, ASSOCHAM, ICC etc. Experienced Software Engineer with a
+                            demonstrated history of working in Computer Software Industry, she has also completed her MBA in Business Analytics and Human Resource in the years
+                            2020. As far as the Analytical expertise is concerned, she is good in data collection, preparation and cleansing and working on a software like Watson,
+                            Cognos, SPSS and Google Analytics. Er. Geetika Pandey is also the Co-Founder and HOD (IT Departt.), SewaPoint. As a social researcher with many years of
+                            experience and understanding of peoples’ needs and requirements, specially related to Governance based on ITES. Being a Technocrat, she has become an
+                            ace in Technical error solving and rectifications. Working on a vision of Women Empowerment, she keeps sensitizing with SHGs and NGOs for creating self-
+                            reliant revenue model. As a philanthropist dedicated towards women empowerment she has travelled from metropolitans to best of cosmopolitans, Tier 2 
+                            cities, small cities, semi-urban areas, suburban regions and even in rural areas situated in the remotest of places to meet for awareness among the BoP 
+                            (bottom of the pyramid) and upliftment of the weakest and backward sections of the society. With her mantra of “Using Science for Serving the Society” she
+                            believes that modern day technology can be the best medium to bridge the gap between the unskilled labor and the employable opportunities.
                             </p>
                         </div>
                     </div>
