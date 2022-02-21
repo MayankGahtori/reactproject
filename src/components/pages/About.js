d27f186f2761dd4f7a40476976be25678a3c86aa
github_username <65722656+MayankGahtori@users.noreply.github.com>
2022-02-21 19:04:00 +0530

    done

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,6 +1,27 @@
 import React from 'react';
-import Slider from '../inc/Slider';
+import Slider1 from '../components/images/Slider1.jpg';
 
-function Aboutus() 
-    
+function Aboutus() {
+    return(
+        <div class="main-slider">
+        <div id="formaSliderCaptions" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src={Slider1}class="d-block w-100" alt="..."/>
+                    <div class="carousel-caption">
+                        <div class="row row-eq-height">
+                            <div class="col-md-8 mt-5">
+                                <h5>Do you want to learn?</h5>
+                                <p>Connecting for <em>Contributing</em>
+                                    Interacting for <span>Innovating</span></p>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+            </div>
+    );
+}
 export default Aboutus;
