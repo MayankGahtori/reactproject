9ee0516a512e927780b894f53f3759d1d2b83dfe
github_username <65722656+MayankGahtori@users.noreply.github.com>
2022-02-21 19:42:06 +0530

    done

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Slider1 from '../images/Slider1.jpg';
-import './App.css';
+
 
 function Aboutus() {
     return(
         <div class="slider">
+          
         <div id="Captions" class="slide" data-ride="carousel">
             <div class="inner">
                 <div class="active">
