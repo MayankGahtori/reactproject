f6359677a8e0673e5f0b6744fbc3c51ac35ac189
github_username <65722656+MayankGahtori@users.noreply.github.com>
2022-02-22 17:57:29 +0530

    done

--- src/components/inc/Slider.js
+++ src/components/inc/Slider.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import Slider1 from '../images/Slider1.jpg';
-
+import Slider2 from '../images/Slider2.jpg';
 function Slider(){
     return(
-        <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src={Slider1} class="d-block w-100" alt="..."/>     
-                </div>
-            </div>
+        <div>
+            <Slider/>
+            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src={Slider1} class="images" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src={Slider2} class="images" alt="..."/>
+    </div>
+    
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
         </div>
-    );
+    )
 }
+
 
 export default Slider;
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,29 +1,10 @@
 import React from 'react';
-import Slider1 from '../images/Slider1.jpg';
-
-
+import Slider from '../inc/Slider';
 function Aboutus() {
     return(
-        <div class="slider">
-          
-        <div id="Captions" class="slide" data-ride="carousel">
-            <div class="inner">
-                <div class="active">
-                    <img src={Slider1}class="d-block w-100" alt="..."/>
-                    <div class="caption">
-                        <div class="row row-eq-height">
-                            <div class="col-md-8 mt-5">
-                                <h5>Do you want to learn?</h5>
-                                <p>Connecting for <em>Contributing</em>
-                                    Interacting for <span>Innovating</span></p>
-                                
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            </div>
-            </div>
+        <div>
+            <Slider/>
+        </div>
     );
 }
 export default Aboutus;
