f335279655394d3b0b8d07f3d3de049b915d86a2
github_username <65722656+MayankGahtori@users.noreply.github.com>
2022-02-23 14:11:10 +0530

    done

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,15 +2,18 @@ import React from 'react';
 import Slider1 from '../images/Slider1.jpg'
 function Aboutus() {
     return(
-      <div class="section">
-  <div class="container">
-    <div class="row">
+      <div className="section">
+  <div className="container">
+    <div className="card-body">
       <img src={Slider1} class="img" alt="..."/>
     </div>
-    <div class="col-md-8">
-      <div class="row">
-        <h5 class="page-heading">Who Are We?</h5>
-        <p class="text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <div className="col-md-9">
+      <div className="row">
+        <h5 className="page-heading">Who Are We?</h5>
+        <p className="text">
+        The Confederation of Global Innovators (also known as CGI) is a platform created<br/>
+        for the demographic dividend of our nation. We strongly believe that today our country’s youth, which comprises more than 65% of the total population, is the biggest asset for our country. From the point of inception, the CGI, as a community, has worked tirelessly in the field of bridging the gap between the stakeholders of development (like students, professional aspirants etc) and the opportunities like the corporate houses. The CGI is created to serve as a platform to connect innovators, stakeholders of development and the student community.
+        </p>
         
       </div>
     </div>
