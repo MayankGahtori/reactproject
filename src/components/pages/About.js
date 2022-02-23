f245cfc7f88f2ad2649189eebe6f70279e6d13e9
github_username <65722656+MayankGahtori@users.noreply.github.com>
2022-02-23 15:22:42 +0530

    don

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,22 +2,31 @@ import React from 'react';
 import Slider1 from '../images/Slider1.jpg'
 function Aboutus() {
     return(
-      <div className="section">
-  <div className="container">
-    <div className="card-body">
-      <img src={Slider1} class="img" alt="..."/>
-    </div>
-    <div className="col-md-9">
-      <div className="row">
-        <h5 className="page-heading">Who Are We?</h5>
-        <p className="text">
-          
-        </p>
-        
-      </div>
-    </div>
-  </div>
-</div>
+      <section className="section">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12" text-left>
+                            <h1 className="main-heading">Who Are We?</h1>
+                            <div className="underline"></div>
+                            <p className="text">
+                                <img src={Slider1} className="image" alt="..." />
+                                The Confederation of Global Innovators (also known as CGI) 
+                                is a platform created for the demographic dividend of our
+                                nation. We strongly believe that today our countryâ€™s
+                                youth, which comprises more than 65% of the total
+                                population, is the biggest asset for our country. From the 
+                                point of inception, the CGI, as a community, has worked
+                                tirelessly in the field of bridging the gap between the
+                                stakeholders of development (like students, professional
+                                aspirants etc) and the opportunities like the corporate
+                                housesThe CGI is created to serve as a platform to connect
+                                innovators, stakeholders of development and the student
+                                community.ðŸ˜Š
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
     );
 }
 export default Aboutus;
