8e110db641256a9fc2e26da6ee50f767ce08be08
github_username <65722656+MayankGahtori@users.noreply.github.com>
2022-02-23 13:49:48 +0530

    done

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,16 +2,16 @@ import React from 'react';
 import Slider1 from '../images/Slider1.jpg'
 function Aboutus() {
     return(
-      <div class="card mb-3">
-  <div class="row g-0">
-    <div class="col-md-4">
+      <div class="section">
+  <div class="container">
+    <div class="row">
       <img src={Slider1} class="img" alt="..."/>
     </div>
     <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      <div class="row">
+        <h5 class="page-heading">Who Are We?</h5>
+        <p class="text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        
       </div>
     </div>
   </div>
