c008f8474718db1daf8343c1857a1498fe27c354
github_username <65722656+MayankGahtori@users.noreply.github.com>
2022-02-23 15:10:46 +0530

    done

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -11,8 +11,7 @@ function Aboutus() {
       <div className="row">
         <h5 className="page-heading">Who Are We?</h5>
         <p className="text">
-        The Confederation of Global Innovators (also known as CGI) is a platform created<br/>
-        for the demographic dividend of our nation. We strongly believe that today our country’s youth, which comprises more than 65% of the total population, is the biggest asset for our country. From the point of inception, the CGI, as a community, has worked tirelessly in the field of bridging the gap between the stakeholders of development (like students, professional aspirants etc) and the opportunities like the corporate houses. The CGI is created to serve as a platform to connect innovators, stakeholders of development and the student community.
+          
         </p>
         
       </div>
