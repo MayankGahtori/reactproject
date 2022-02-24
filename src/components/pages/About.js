0afa275752d0deb28bc8e1202bec1046baf585f6
github_username <65722656+MayankGahtori@users.noreply.github.com>
2022-02-24 09:09:46 +0530

    done

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -9,6 +9,7 @@ import Slider7 from '../images/Slider7.jpg'
 import Slider8 from '../images/Slider8.jpg'
 import Slider9 from '../images/Slider9.jpg'
 import nilesh from '../images/nilesh.jpg'
+import geetika from '../images/geetika.jpg'
 function Aboutus() {
     return(
       <section className="section">
@@ -100,10 +101,70 @@ function Aboutus() {
                 <div className="container">
                     <div className="row">
                         <div class="col-sm-9">
+                            <p className="text">
+                                <div className="images">
+                                    <span className="person-image mb-2">
+                                        <img src={nilesh} alt="srcset"/>
+                                    </span>
+                                    <p className="personName">Nilesh Sinha</p>
+                                    <p className="g">Chairman</p>
+                                </div>
+                            With his 1st startup at an age of 14 years, Mr. Nilesh Sinha has an experience of working with 
+                            many startups. As an E-governance and Financial Inclusion professional with years of experience 
+                            in the ecosystem, especially in India, he is specialized in networking and communicating with 
+                            institutions and authorities. Recipient of 3rd position in the International Innovation Fair 2017, 
+                            Vizag, Mr. Sinha has been regularly invited as a guest speaker and panelist for various corporate 
+                            events and panel discussions. In his unique approach, he emphasizes on empowering the innovators. 
+                            He says that a true innovator can be anyone from any professional, geographical or other background. 
+                            In his version, a five-year-old kid practicing python to a 67 years old Colonel Sanders opening 
+                            world’s most liked chicken restaurant chain, every person with a unique approach or solution is an 
+                            innovator. Also being a trained Hindustani Classical singer, Mr. Sinha promotes living a life with a
+                            balance among all the aspects of life including the professional commitments, paying proper attentions
+                            towards your hobbies to respecting your friends and family members. This is what he considers as his 
+                            success mantra for wining many National level competitions and titles. He regularly writes blogs and 
+                            articles based on the current startup and corporate ecosystem as well as on latest academic and 
+                            technological trends on global perspective (like: Blockchain, IoT, Big Data etc.) On his mission of 
+                            Gamification of Innovation, he has so far been a guest speaker at 40+ schools, colleges and educational 
+                            institutions impacting the life of more than Sixty thousand learners and innovators.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+        </section>
+        <section className="section">
+                <div className="container">
+                    <div className="row">
+                        <div class="col-sm-9">
                             
-                            <p className="text"></p>
-                                
-                                With his 1st startup at an age of 14 years, Mr. Nilesh Sinha has an experience of working with many startups. As an E-governance and Financial Inclusion professional with years of experience in the ecosystem, especially in India, he is specialized in networking and communicating with institutions and authorities. Recipient of 3rd position in the International Innovation Fair 2017, Vizag, Mr. Sinha has been regularly invited as a guest speaker and panelist for various corporate events and panel discussions. In his unique approach, he emphasizes on empowering the innovators. He says that a true innovator can be anyone from any professional, geographical or other background. In his version, a five-year-old kid practicing python to a 67 years old Colonel Sanders opening world’s most liked chicken restaurant chain, every person with a unique approach or solution is an innovator. Also being a trained Hindustani Classical singer, Mr. Sinha promotes living a life with a balance among all the aspects of life including the professional commitments, paying proper attentions towards your hobbies to respecting your friends and family members. This is what he considers as his success mantra for wining many National level competitions and titles. He regularly writes blogs and articles based on the current startup and corporate ecosystem as well as on latest academic and technological trends on global perspective (like: Blockchain, IoT, Big Data etc.) On his mission of Gamification of Innovation, he has so far been a guest speaker at 40+ schools, colleges and educational institutions impacting the life of more than Sixty thousand learners and innovators.
+                            <p className="text">
+                                <div className="images">
+                                    <span className="person-image mb-2">
+                                        <img src={geetika} alt="srcset"/>
+                                    </span>
+                                    <p className="personName">Er. Geetika Pandey</p>
+                                    <p className="g">President</p>
+                                </div>
+                                Completed Btech-CSE in the year 2016 and has the vibrant experience in IT field. 
+                                Also has 3+ years’ experience in corporate world. Her goal is to hold a Senior 
+                                Corporate position along with being a Philanthropist. She had a very sound communication 
+                                and Technical skills and regularly participate as an invited/guest delegate in many IT Conclave, 
+                                Summit and Sessions organized by FICCI, CII, ASSOCHAM, ICC etc. Experienced Software Engineer 
+                                with a demonstrated history of working in Computer Software Industry, she has also completed 
+                                her MBA in Business Analytics and Human Resource in the year 2020. As far as the Analytical 
+                                expertise is concerned, she is good in data collection, preparation and cleansing and working 
+                                on a software like Watson, Cognos, SPSS and Google Analytics. Er. Geetika Pandey is also the 
+                                Co-Founder and HOD (IT Departt.), SewaPoint. As a social researcher with many years of experience 
+                                and understanding of peoples’ needs and requirements, specially related to Governance based on 
+                                ITES. Being a Technocrat, she has become an ace in Technical error solving and rectifications. 
+                                Working on a vision of Women Empowerment, she keeps sensitizing with SHGs and NGOs for creating 
+                                self-reliant revenue model. As a philanthropist dedicated towards women empowerment she has 
+                                travelled from metropolitans to best of cosmopolitans, Tier 2 cities, small cities, semi-urban 
+                                areas, suburban regions and even in rural areas situated in the remotest of places to meet for 
+                                awareness among the BoP (bottom of the pyramid) and upliftment of the weakest and backward 
+                                sections of the society. With her mantra of “Using Science for Serving the Society” she believes 
+                                that modern day technology can be the best medium to bridge the gap between the unskilled labor 
+                                and the employable opportunities
+                            </p>
                         </div>
                     </div>
                 </div>
