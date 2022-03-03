aae274a8e9653a5d732dbdfed054b9005d460767
github_username <65722656+MayankGahtori@users.noreply.github.com>
2022-03-03 14:39:16 +0530

    done

--- src/components/inc/Navbar.js
+++ src/components/inc/Navbar.js
@@ -2,18 +2,39 @@ import React from 'react';
 
 function Navbar() {
     return(
-      <ul class="nav justify-content-end">
-      <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="/">Active</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/">Link</a>
-      </li>
-     
-    </ul>
+      <section className='navbar-bg gradient'>
+      <nav class="navbar navbar-expand-lg navbar-light ">
+          <div class="container">
+              {/* <a class="navbar-brand" href="#">Navbar</a> */}
+              <img src={IMG} class="img-fluid"></img>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                      <li class="nav-item">
+                          <a class="nav-link active" aria-current="page" href="/">Home</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link color" href="/">About</a>
+                      </li>
+                      <li class="nav-item ">
+                          <a class="nav-link color" href="/">Services</a>
+                      </li>
+                      <li class="nav-item  targeT">
+                          <a class="nav-link color" href="/">FeedBack</a>
+                      </li>
+                  </ul>
+                  <form class="d-flex">
+                      {/* <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input> */}
+                      <button class="btn  btn-style btn-style2" type="submit">Login</button>
+                      {/* <button class="btn  btn-style btn-style2" type="submit">About</button> */}
+                      <button class="btn  btn-style" type="submit">Signup</button>
+                  </form>
+              </div>
+          </div>
+      </nav>
+  </section>
     );
 }
 
