213e45c3c44cdbd8033bf2862f67fec0ce763f10
github_username <65722656+MayankGahtori@users.noreply.github.com>
2022-03-04 18:49:39 +0530

    done

--- src/components/inc/Navbar.js
+++ src/components/inc/Navbar.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import R from '../images/R.png';
 function Navbar() {
     return(
-      <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="/">
-          <img src={R} alt="Avatar Logo"  class="rounded-pill"/> 
-        </a>
+      <div>
+        
       </div>
-    </nav>
     );
 }
 
