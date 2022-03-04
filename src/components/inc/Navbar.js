d31cf0d031bdd0305481f49893b957da4672f44a
github_username <65722656+MayankGahtori@users.noreply.github.com>
2022-03-04 18:09:32 +0530

    done

--- src/components/inc/Navbar.js
+++ src/components/inc/Navbar.js
@@ -1,41 +1,29 @@
 import React from 'react';
-import R from '../images/R.png';
 
 function Navbar() {
     return(
-      <section className='navbar-bg gradient'>
-      <nav class="navbar navbar-expand-lg navbar-light ">
-          <div class="container">
-              {/* <a class="navbar-brand" href="#">Navbar</a> */}
-              <img src={R} alt=""/>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                  <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                      <li class="nav-item">
-                          <a class="nav-link active" aria-current="page" href="/">Home</a>
-                      </li>
-                      <li class="nav-item">
-                          <a class="nav-link color" href="/">About</a>
-                      </li>
-                      <li class="nav-item ">
-                          <a class="nav-link color" href="/">Services</a>
-                      </li>
-                      <li class="nav-item  targeT">
-                          <a class="nav-link color" href="/">FeedBack</a>
-                      </li>
-                  </ul>
-                  <form class="d-flex">
-                      {/* <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input> */}
-                      <button class="btn  btn-style btn-style2" type="submit">Login</button>
-                      {/* <button class="btn  btn-style btn-style2" type="submit">About</button> */}
-                      <button class="btn  btn-style" type="submit">Signup</button>
-                  </form>
-              </div>
-          </div>
+        <nav class="navbar navbar-expand-lg navbar-dark primary-color">
+        <a class="navbar-brand" href="/">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/">Features</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/">Pricing</a>
+            </li>
+           
+          </ul>
+        </div>
       </nav>
-  </section>
+        
     );
 }
 
