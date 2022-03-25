e9d931c10d7291c99d0b55d37a1e1c36f267d110
github_username <65722656+MayankGahtori@users.noreply.github.com>
2022-03-25 18:51:44 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './App.css';
-
+import Dashboard from './Dashboard';
 import About from './components/pages/About';
-
 import {BrowserRouter as Router} from 'react-router-dom';
 function App() {
   return (
     <Router>
       <div>
-      
+      <Dashboard/>
         <About />
       </div>
     </Router>
