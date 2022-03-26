96a3549334f990b3e3b1aa50ffe689e26f67e058
github_username <65722656+MayankGahtori@users.noreply.github.com>
2022-03-26 09:57:08 +0530

    done

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import reportWebVitals from './reportWebVitals';
 import App from './App';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle';
+
 ReactDOM.render(
   <React.StrictMode>
    <App />
